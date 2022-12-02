470a142318d376cf326342b121ef72a24177d7e2
Bryan Kim <github@bryankim.dev>
2022-12-02 22:15:51 +0900

    Set up url structure

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+# Main app
+urlpatterns += [
+    path('', include('geultarae.urls'))
+]
+
+# Use static() to add URL mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Add Django site authentication urls (for login, logout, password management)
+
+urlpatterns += [
+    path('', include('django.contrib.auth.urls')),
 ]
