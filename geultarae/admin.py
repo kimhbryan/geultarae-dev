91f6d0b9496f378af8d0d8c0bc87fea428dffdd5
Bryan Kim <github@bryankim.dev>
2022-12-02 22:19:11 +0900

    Create admin and register models

--- geultarae/admin.py
+++ geultarae/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import UserCreateForm, UserModifyForm
+from .models import User
 
-# Register your models here.
+
+class Admin(UserAdmin):
+    add_form = UserCreateForm
+    form = UserModifyForm
+    model = User
+    list_display = ["email", "username",]
+
+
+admin.site.register(User, Admin)
