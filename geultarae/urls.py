f15b74492c80f87c911c18559d2c683d92481439
Bryan Kim <github@bryankim.dev>
2022-12-26 20:44:14 +0900

    Add mypage url

--- geultarae/urls.py
+++ geultarae/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('ask/', views.ask, name='ask'),
+    path('mypage/', views.mypage, name='mypage'),
 ]
