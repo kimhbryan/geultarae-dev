8c1d557aebb77da09810d6d671ab811ac97d3e50
Bryan Kim <github@bryankim.dev>
2023-01-07 14:16:50 +0900

    Add generic writing url

--- geultarae/urls.py
+++ geultarae/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('mypage/', views.mypage, name='mypage'),
     path('writing1/', views.writing1, name='writing1'),
     path('writing2/', views.writing2, name='writing2'),
+    path('writing/<int:pk>/', views.writing.as_view(), name='writing'),
 ]
