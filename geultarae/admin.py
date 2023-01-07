9e65f74360a3f55a265bff709ed92e323b884e9d
Bryan Kim <github@bryankim.dev>
2023-01-07 14:17:12 +0900

    Register writing model

--- geultarae/admin.py
+++ geultarae/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .forms import UserCreateForm, UserModifyForm
-from .models import User
+from .models import User, Writing
 
 
 class Admin(UserAdmin):
@@ -12,3 +12,8 @@ class Admin(UserAdmin):
 
 
 admin.site.register(User, Admin)
+
+
+@admin.register(Writing)
+class WritingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'hint', 'date_available', 'is_available')
