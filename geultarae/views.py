a33478d9ac1007594d8b40bb919bd1defc1c777f
Bryan Kim <github@bryankim.dev>
2023-01-07 14:16:59 +0900

    Add writing detail view

--- geultarae/views.py
+++ geultarae/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views import generic
 
+from .models import Writing
 
 @login_required
 def index(request):
@@ -25,3 +27,7 @@ def writing1(request):
 @login_required
 def writing2(request):
     return render(request, 'writing2.html')
+
+class writing(generic.DetailView):
+    model = Writing
+    template_name = 'writing.html'
