a2b1aade2576933b6d0edff8bb32624d012503d8
Bryan Kim <github@bryankim.dev>
2023-02-08 12:16:53 +0900

    Add SMTP settings (TODO)

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,11 @@ LOGOUT_REDIRECT_URL = '/'
 
 # Use console email backend
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# TODO: Use SMTP email backend
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'skeinofwords@gmail.com'
+# EMAIL_HOST_PASSWORD = ''
