47dbaa4f625a87fcaa38c1f04d14126f9cd22f39
Bryan Kim <github@bryankim.dev>
2023-02-10 21:12:31 +0900

    Add ask Form

--- geultarae/forms.py
+++ geultarae/forms.py
@@ -13,3 +13,9 @@ class UserModifyForm(UserChangeForm):
     class Meta:
         model = User
         fields = ("username", "email")
+
+
+class AskForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    from_email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
