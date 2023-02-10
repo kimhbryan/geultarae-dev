7f8ce450cf079ce072b16a37ea034d81118bd7b7
Bryan Kim <github@bryankim.dev>
2023-02-10 21:12:18 +0900

    Switch to SMTP email backend

--- config/settings.py
+++ config/settings.py
@@ -138,12 +138,12 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # Use console email backend
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # TODO: Use SMTP email backend
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'skeinofwords@gmail.com'
-# EMAIL_HOST_PASSWORD = ''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'skeinofwords@gmail.com'
+EMAIL_HOST_PASSWORD = 'sgeqlckuhfaxyycb'
