2c56be7a3d84d7ae824c6909fbbad4f43279423d
Bryan Kim <github@bryankim.dev>
2023-02-10 20:26:55 +0900

    Add plot url

--- geultarae/urls.py
+++ geultarae/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('ask/', views.ask, name='ask'),
     path('mypage/', views.mypage, name='mypage'),
     path('writing/<int:pk>/', views.writing, name='writing'),
+    path('plot/<int:pk>/', views.plot, name='plot')
 ]
