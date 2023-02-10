537790a57420383ad997336cb57c612ba47bc2a0
Bryan Kim <github@bryankim.dev>
2023-02-10 21:13:30 +0900

    Add ask_done url

--- geultarae/urls.py
+++ geultarae/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('ask/', views.ask, name='ask'),
+    path('ask/done/', views.ask_done, name='ask_done'),
     path('mypage/', views.mypage, name='mypage'),
     path('writing/<int:pk>/', views.writing, name='writing'),
     path('plot/<int:pk>/', views.plot, name='plot')
